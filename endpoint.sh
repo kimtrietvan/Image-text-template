fb47457c16966b9248cbeb0c02222506bc577654
Trường Đinh <truongdinh@192.168.1.2>
2023-08-23 00:16:11 +0700

    .

--- endpoint.sh
+++ endpoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo node --harmony /root/app/node-file-manager/lib/index.js -p 81 -d /root/app &
+sudo node --harmony /root/app/node-file-manager/lib/index.js -p 81 -d /root/app & > output.txt &
 # python3 /root/app/main.py &
 sudo gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80 &> output.txt &
