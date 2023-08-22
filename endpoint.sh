316874624dd541d2bff2e4f531694feaf4ebb354
Trường Đinh <truongdinh@192.168.1.2>
2023-08-23 00:31:20 +0700

    ,

--- endpoint.sh
+++ endpoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80 &> output.txt &
+#sudo gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80 &> output.txt &
 
-cd node-file-manager && npm i &&sudo npm run start &> output.txt &
+#cd node-file-manager && npm i && sudo npm run start &> output.txt &
